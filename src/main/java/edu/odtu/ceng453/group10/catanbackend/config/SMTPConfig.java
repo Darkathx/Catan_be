1d91b321065d4fa571fce2d9c192b0fc8691eeea
Meriç Kerem Yalçın <merickereme@gmail.com>
2023-11-10 20:16:19 +0300

    reset password link generation

--- src/main/java/edu/odtu/ceng453/group10/catanbackend/config/SMTPConfig.java
+++ src/main/java/edu/odtu/ceng453/group10/catanbackend/config/SMTPConfig.java
@@ -8,20 +8,16 @@ import java.util.Properties;
 
 public final class SMTPConfig {
     public static Properties PROP = new Properties();
+    public static String URL = "";
     static {
-        PROP.put("mail.smtp.auth", true);
-        PROP.put("mail.smtp.starttls.enable", "true");
-        PROP.put("mail.smtp.host", "");
+        //PROP.put("mail.smtp.auth", true);
+        //PROP.put("mail.smtp.starttls.enable", "true");
+        PROP.put("mail.smtp.host", "localhost");
         PROP.put("mail.smtp.port", "25");
-        PROP.put("mail.smtp.ssl.trust", "");
+        //PROP.put("mail.smtp.ssl.trust", "");
     }
 
     public static Session getSession() {
-        return Session.getInstance(PROP, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("username", "password");
-            }
-        });
+        return Session.getDefaultInstance(PROP);
     }
 }
